24da87bfe2c98f93285b1b478b97cee0089b8464
Danish Kalesaran <danishkalesaran@gmail.com>
2021-02-12 23:08:42 +0700

    Add blog_web phoenix app

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,17 @@
 # move said applications out of the umbrella.
 import Config
 
+config :blog_web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :blog_web, BlogWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "NTh1hWIsmmut0f9CMsJ/VvMIUVKYJbJ2qDcrv91TQARymRO8JT9OCYeuhAhrY1dD",
+  render_errors: [view: BlogWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: BlogWeb.PubSub,
+  live_view: [signing_salt: "833f2ja6"]
+
 # Sample configuration:
 #
 #     config :logger, :console,
@@ -16,3 +27,15 @@ import Config
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
