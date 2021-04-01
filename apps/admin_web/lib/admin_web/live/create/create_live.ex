a77d891941a42247803ca639a973638cee44d0db
Danish Kalesaran <danishkalesaran@gmail.com>
2021-04-01 19:16:29 +0700

    Use Hypermarkdown to parse markdown to html

--- apps/admin_web/lib/admin_web/live/create/create_live.ex
+++ apps/admin_web/lib/admin_web/live/create/create_live.ex
@@ -1,3 +1,21 @@
 defmodule AdminWeb.CreateLive do
   use AdminWeb, :live_view
+  alias Hypermarkdown
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, %{preview: %{title: "", content: ""}})}
+  end
+
+  @impl true
+  def handle_event("title-update", %{"value" => title}, socket) do
+    preview = %{socket.assigns.preview | title: title}
+    {:noreply, assign(socket, %{preview: preview})}
+  end
+
+  @impl true
+  def handle_event("content-update", %{"value" => content}, socket) do
+    preview = %{socket.assigns.preview | content: Hypermarkdown.parse(:md_to_html, content)}
+    {:noreply, assign(socket, %{preview: preview})}
+  end
 end
